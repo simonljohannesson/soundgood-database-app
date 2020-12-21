17a0df0229633534be1d42fc2f771572256f43ef
Simon Johannesson <simonljohannesson@gmail.com>
2020-12-21 23:17:51 +0100

    Added google test example.

---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+//
+// Created by simon on 2020-12-21.
+//
+
+#include "test.h"
+#include "gtest/gtest.h"
+
+TEST (ExampleTest, First){
+    SUCCEED();
+}
+
+TEST (ExampleTest, Second){
+    FAIL();
+}
---
+++ test/test.h
@@ -0,0 +1,14 @@
+//
+// Created by simon on 2020-12-21.
+//
+
+#ifndef SOUNDGOOD_TEST_H
+#define SOUNDGOOD_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //SOUNDGOOD_TEST_H
