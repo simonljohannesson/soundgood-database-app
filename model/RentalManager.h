94b7e3ef5b5874509b963f71b6f60bd8202f65f1
Simon Johannesson <simonljohannesson@gmail.com>
2020-12-29 23:12:04 +0100

    Added documentaion.

--- model/RentalManager.h
+++ model/RentalManager.h
@@ -14,21 +14,58 @@ public:
     std::vector<dto::RentalInstrument> RequestAvailableRentalInstruments(
             const std::string &instrument_type,
             std::shared_ptr<integration::DbHandler> &db_handler);
-
+    /**
+     * Tries to start a rental for a student with the chosen instrument_id.
+     *
+     * Can throw:
+     * - integration::DatabaseError
+     * - model::StudentNotEnrolledError
+     * - model::NumberAllowedRentalsExceededError
+     * - model::InstrumentNotAvailableError
+     * @param student_id
+     * @param instrument_id
+     * @param db_handler
+     */
     void RentInstrument(
             int student_id,
             const std::string &instrument_id,
             std::shared_ptr<integration::DbHandler> &db_handler);
-
+    /**
+     * Checks if a rental is active.
+     *
+     * Can throw:
+     * - integration::DatabaseError
+     * - model::RentalNotActiveError
+     * @param rental_id id of rental to terminate
+     * @param student_id id of student associated to the rental to terminate
+     * @param db_handler database handler used
+     */
     void TerminateRental(
             int rental_id,
             int student_id,
             std::shared_ptr<integration::DbHandler> &db_handler);
-
+    /**
+     * Query database for all active rentals associated to a student.
+     *
+     * Can throw integration::DatabaseError.
+     *
+     * @param student_id id of student
+     * @param db_handler
+     * @return std::vector of active rentals
+     */
     std::vector<dto::ActiveRental> GetActiveRentals(
             int student_id,
             std::shared_ptr<integration::DbHandler> &db_handler);
-
+    /**
+     * Returns the student id associated with a username.
+     *
+     * Can throw integration::DatabaseError.
+     *
+     * Can also throw integration::InvalidUsernameError if username cannot be found.
+     * @param username
+     * @param db_handler
+     * @return int
+     */
     int GetStudentIdFromUsername(
             const std::string &username,
             std::shared_ptr<integration::DbHandler> &db_handler);
